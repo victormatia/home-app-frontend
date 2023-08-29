11fa9cacc3f55c27af65bb10440379cd521b28bc
Matheus Araújo <matheusfoaraujo@gmail.com>
2023-08-29 16:04:17 -0300

    feat: adds user button and a search bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import { UserButton, auth } from '@clerk/nextjs';
+import Image from 'next/image';
 import Link from 'next/link';
+import Lupa from '../assets/Lupa.svg';
 
 const links = [
   { name: 'Alugar', href: '/rent' },
@@ -10,18 +12,38 @@ const links = [
 const Header = async () => {
   const { userId } = await auth();
   return(
-    <div className='flex justify-between items-center w-full px-5'>
-      <div>
+    <div className='flex px-6 py-7 gap-11 items-center justify-center bg-app'>
+      <div className="max-[500px]:hidden">
         <Link href='/'> Logo </Link>
       </div>
-      <div className='flex gap-2 items-center' >
-        <ul className="px-4 flex gap-2">
+
+      <label htmlFor="search" className='flex gap-2 items-center w-[285px] h-8 rounded bg-white'>
+        <input 
+          type="text" 
+          name="search" 
+          id="search" 
+          className='
+          bg-transparent 
+          w-[250px] h-6 py-2 px-3 border-r border-r-placeholder
+          text-xs placeholder:text-placeholder'
+          placeholder='Rua 1, Número 2, Bairro 3...'
+        />
+        <button type="submit">
+          <Image src={Lupa} alt='lupa'/>
+        </button>
+      </label>
+
+      <div className='max-[500px]:hidden' >
+        <ul className="">
           {links.map((link) => (
             <li key={link.name} className="text-xl my-4">
               <Link href={link.href}>{link.name}</Link>
             </li>
           ))}
         </ul>
+      </div>
+      
+      <div>
         {
           userId === null ?
             <Link href='/sign-in' >
@@ -30,6 +52,7 @@ const Header = async () => {
             <UserButton afterSignOutUrl="/"/>
         }
       </div>
+      
     </div>
   );
 };
