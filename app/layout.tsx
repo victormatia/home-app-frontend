69da599c5f251620b9c4f09a1fa375975e8d8207
matheusfoaraujo <73747862+matheusfoaraujo@users.noreply.github.com>
2023-09-08 17:02:38 -0300

    Revert "[MATHEUS ARAUJO] Changes auth technology"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import Header from '@/components/Header';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Header from '@/components/Header';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,13 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <UserProvider>
+    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
           <Header />
           {children}
         </body>
       </html>
-    </UserProvider>
+    </ClerkProvider>
   );
 }
--- next.config.js
+++ next.config.js
@@ -8,18 +8,6 @@ const nextConfig = {
         port: '',
         pathname: '/*',
       },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/a/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 's.gravatar.com',
-        port: '',
-        pathname: '/avatar/**',
-      },
     ],
   },
 };
