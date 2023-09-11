dc985f206002bc989b07454aaa0d981d92713e2e
Guilherme Ciarlini <guilhermeciarlini@gmail.com>
2023-09-11 15:23:24 -0300

    feat: add phosphor-react, color extension in tailwind.config  and change header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,24 @@
 'use client';
-import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from 'next/image';
 import Link from 'next/link';
-import {
-  Nav,
-  NavItem,
-} from 'reactstrap';
 import Lupa from '../assets/Lupa.svg';
+import { Sliders } from 'phosphor-react';
 
 const links = [
   { name: 'Alugar', href: '/rent' },
   { name: 'Comprar', href: '/buy' },
   { name: 'Anunciar', href: '/advertise' },
 ];
+
 const Header = () => {
-  const { user, isLoading } = useUser();
-  console.log(user);
+  
   return(
     <div className='flex h-20 px-6 py-7 gap-11 items-center justify-center bg-app'>
       <div className="max-[500px]:hidden">
         <Link href='/'> Logo </Link>
       </div>
 
-      <label htmlFor="search" className='flex gap-2 items-center w-[285px] h-8 rounded bg-white'>
+      <label htmlFor="search" className='flex gap-2 items-center w-[285px] px-2 h-8 rounded bg-white'>
         <input 
           type="text" 
           name="search" 
@@ -47,53 +43,11 @@ const Header = () => {
           ))}
         </ul>
       </div>
-      
       <div>
-        {!isLoading && !user && (
-          <Nav className="d-md-none" navbar>
-            <a
-              href="/api/auth/login"
-              className="btn btn-primary btn-block"
-              tabIndex={0}>
-                  Log in
-            </a>
-          </Nav>
-        )}
-        {user && (
-          <Nav
-            id="nav-mobile"
-            className="d-md-none justify-content-between"
-            navbar
-            data-testid="navbar-menu-mobile">
-            <NavItem>
-              <span className="user-info">
-                <Image
-                  src={user.picture as string}
-                  alt="Profile"
-                  className="nav-user-profile d-inline-block rounded-circle mr-3"
-                  width="50"
-                  height="50"
-                  data-testid="navbar-picture-mobile"
-                />
-                <h6 className="d-inline-block" data-testid="navbar-user-mobile">
-                  {user.name}
-                </h6>
-              </span>
-            </NavItem>
-            <NavItem>
-              <a href="/profile">
-                    Profile
-              </a>
-            </NavItem>
-            <NavItem id="qsLogoutBtn">
-              <a
-                href="/api/auth/logout"
-                className="btn btn-link p-0">
-                    Log out
-              </a>
-            </NavItem>
-          </Nav>
-        )}
+        <button className='flex flex-row justify-between items-center text-placeholder'>
+          <Sliders size={24} className='rotate-90'/>
+          Filter
+        </button>
       </div>
     </div>
   );
