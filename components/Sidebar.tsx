f5b9684cbda9dfb118b38e7341965a2fb7489004
victormatia <fvictor1705@gmail.com>
2023-10-20 15:18:14 -0300

    style: styles Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,8 +13,8 @@ export default function Sidebar() {
 
   return (
     <section
-      // eslint-disable-next-line max-len
-      className='max-[700px]:hidden h-screen w-20 flex flex-col justify-between items-center absolute top-0 left-0 bg-darkGray py-6'  
+      className='max-[700px]:hidden
+      h-screen w-20 flex flex-col justify-between items-center absolute top-0 left-0 bg-darkGray py-6'  
     >
       <section>
         <Image src={homeIcon} width={32} height={32} alt={''} />
@@ -22,22 +22,26 @@ export default function Sidebar() {
 
       <section className='h-64 w-full flex flex-col items-center justify-center gap-6 text-zinc-500 text-2xl'>
         <Link
-          className='relative w-full h-12 flex justify-center items-center'
+          className='relative w-full h-12 flex justify-center items-center sidebar__span'
           href='/'
           onClick={() => setCurrPage('home')}
+          data-value='Pesquisar'
         >
           {
             currPage === 'home'
             &&  <div className='absolute top-0 left-0 h-full w-1 bg-paymentButton rounded-r-md' />
           }
           
-          <SlMagnifier className="hover:text-zinc-200" />
+          <SlMagnifier
+            className="hover:text-zinc-200"
+          />
         </Link>
 
         <Link
-          className='relative w-full h-12 flex justify-center items-center'
+          className='relative w-full h-12 flex justify-center items-center sidebar__span'
           href='/saved'
           onClick={() => setCurrPage('saved')}
+          data-value='Salvos'
         >
           {
             currPage === 'saved'
@@ -48,9 +52,10 @@ export default function Sidebar() {
         </Link>
 
         <Link
-          className='relative w-full h-12 flex justify-center items-center'
+          className='relative w-full h-12 flex justify-center items-center sidebar__span'
           href='/advertise'
           onClick={() => setCurrPage('advertise')}
+          data-value='Anúnciar'
         >
           {
             currPage === 'advertise' 
@@ -63,9 +68,10 @@ export default function Sidebar() {
 
       <section className='w-full text-zinc-500 text-2xl'>
         <Link
-          className='relative w-full h-12 flex justify-center items-center'
+          className='relative w-full h-12 flex justify-center items-center sidebar__span'
           href='/settings'
           onClick={() => setCurrPage('settings')}
+          data-value='Configurações'
         >
           {
             currPage === 'settings' 
