f60fa8d429a8864888d69e6d06e1eca1b21ba0f2
victormatia <fvictor1705@gmail.com>
2023-11-10 16:33:27 -0300

    refactor: implements react-query logic and refactor login authentication

--- context/GlobalProvider.tsx
+++ context/GlobalProvider.tsx
@@ -1,63 +1,65 @@
 'use client';
 
-import { ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
+import { ReactNode, useMemo, useState } from 'react';
 import globalContext from './context';
-import { useUser } from '@auth0/nextjs-auth0/client';
 import axios from 'axios';
-import getAuthUsers from '@/utils/getAuthUsers';
 import { TImmobile, TRankedImmobile } from '@/types';
+import { useQuery } from '@tanstack/react-query';
+import { Session } from '@auth0/nextjs-auth0';
 
 export default function GlobalProvider({ children }: { children: ReactNode }) {
-  const { user } = useUser();
   const [immobiles, setImmobiles] = useState<TImmobile[]>([]);
   const [searchedImmobiles, setSearchedImmobiles] = useState<TRankedImmobile[]>([]);
   const [search, setSearch] = useState<string>('');
   const [currPage, setCurrPage] = useState<string>('home');
   const [toggleOpenFilter, setToggleOpenFilter] = useState<boolean>(false);
-  
-  (useCallback(async () => {
-    if (user) {
-      const [{ user_id, name, email, created_at, updated_at }] = await getAuthUsers(user.email as string);
+
+  const {} = useQuery({
+    queryKey: ['immobiles'],
+    queryFn: async () => {
+      const { data } = await axios.get('http://localhost:3001/immobile/list');
+      setImmobiles(data);
+      setSearchedImmobiles(data.map((i: TImmobile) => ({ immobileId: i.id,  immobile: i, rank: 0 })));
+
+      return data;
+    },
+  });
+
+  const { data: session } = useQuery<Session>({
+    queryKey: ['session'],
+    queryFn: async () => {
+      const { data } = await axios.get('http://localhost:3000/api/auth/sessionInfos');
       
-      const URL = 'http://localhost:3001/user/sign-up';
+      return data;
+    },
+  });
+  
+  useMemo(async () => {
+    if (session?.user) {
+      const SIGN_IN_URL = 'http://localhost:3001/user/sign-in';
+      const SIGN_UP_URL = 'http://localhost:3001/user/sign-up';
 
       const userData = {
-        id:user_id,
-        name:name,
-        email: email,
-        createdAt:created_at,
-        updatedAt:updated_at,
+        name: session.user.name,
+        email: session.user.email,
+        updatedAt: session.user.updated_at,
       };
 
-      const { data } = await axios.get('http://localhost:3001/user/list');
-
-      const userAlreadyRegistered = data.some(({ id }: {id: string}) => id === user_id);
-
-      if (!userAlreadyRegistered) {
-        
-        axios.post(URL, userData)
-          .then((response) => {
-            sessionStorage.setItem('token', response.data.token);
-            localStorage.setItem('token', response.data.token);
-            console.log(response.data.token);
-          })
-          .catch((e) => {
-            console.log(e);
-          });
-      }
-    }
-  }, [user]))();
-
-  useEffect(() => {
-    (async () => {
-      axios.get('http://localhost:3001/immobile/list')
-        .then(({ data }) => {
-          setImmobiles(data);
-          setSearchedImmobiles(data.map((i: TImmobile) => ({ immobileId: i.id,  immobile: i, rank: 0 })));
+      axios.post(SIGN_IN_URL, { email: session.user.email })
+        .then((response) => {
+          localStorage.setItem('token', response.data.token);
         })
-        .catch((e) => console.error(e));
-    })();
-  }, []);
+        .catch(() => {
+          axios.post(SIGN_UP_URL, userData)
+            .then((response) => {
+              localStorage.setItem('token', response.data.token);
+            })
+            .catch((e) => {
+              console.error(e);
+            });
+        });
+    }
+  }, [session]);
 
   const states = useMemo(() => ({
     immobiles, setImmobiles,
@@ -65,6 +67,7 @@ export default function GlobalProvider({ children }: { children: ReactNode }) {
     searchedImmobiles, setSearchedImmobiles,
     currPage, setCurrPage, toggleOpenFilter, setToggleOpenFilter,
   }), [immobiles, searchedImmobiles, search, currPage, toggleOpenFilter]);
+
   return (
     <globalContext.Provider value={ states }>
       { children }
