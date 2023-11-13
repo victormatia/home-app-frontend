93e25ea33721e01acb25fea9bef40bafcee01d1a
Guilherme Ciarlini <guilhermeciarlini@gmail.com>
2023-11-13 16:44:12 -0300

    feat: create most used filters logic

--- components/MostUsedFilters.tsx
+++ components/MostUsedFilters.tsx
@@ -1,15 +1,42 @@
-const MostUsedFiltersListMock = ['destaques', 'aceita pet', '2 quartos', '3 quartos'];
+import globalContext from '@/context/context';
+import { useContext } from 'react';
+
+const mosteUsedFilters = [
+  { name: 'Quartos', key:  'bedroomsQty', value: 3 },
+  { name: 'Quartos', key:  'bedroomsQty', value: 2 },
+  { name: 'Aceita pet', key:  'petFriendly', value: true },
+];
 
 export default function MostUsedFilters() {
+
+  const { setPropertyCaracteristics } = useContext(globalContext);
+
+
   return (
     <ul className='flex gap-3'>
+      
+      <li className="rounded-2xl bg-[#C1C1C1] text-white py-1 px-3">
+        <button>
+          destaques
+        </button>
+      </li>
+
       {
-        MostUsedFiltersListMock.map((filter, i) => (
-          <li key={filter + i} className="rounded-2xl bg-[#C1C1C1] text-white py-1 px-3">
-            <button>{filter}</button>
+        mosteUsedFilters.map(filter => (
+          <li key={filter.key + filter.value} className="rounded-2xl bg-[#C1C1C1] text-white py-1 px-3">
+            <button onClick={() => {
+              
+              setPropertyCaracteristics((prev) => ({
+                ...prev,
+                [filter.key] : filter.value,
+              }));
+            }}>
+              { `${typeof filter.value === 'number'? filter. value : ''} ${filter.name}`}
+            </button>
           </li>
         ))
       }
+     
     </ul>
   );
 }
