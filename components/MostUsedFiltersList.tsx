9071141e17d60c46ed42b4c713099adf777ba1ee
victormatia <fvictor1705@gmail.com>
2023-11-16 17:55:05 -0300

    style: adds divider and font-weight

--- components/MostUsedFiltersList.tsx
+++ components/MostUsedFiltersList.tsx
@@ -10,13 +10,15 @@ const mosteUsedFilters = [
 export default function MostUsedFiltersList() {
 
   return (
-    <ul className='flex gap-3'>
+    <ul className='flex items-center gap-3'>
       
-      <li className="rounded-2xl bg-[#C1C1C1] text-white py-1 px-3">
+      <li className="rounded-2xl bg-gradientBlue text-white py-1 px-3 font-medium">
         <button>
-          destaques
+          Destaques
         </button>
       </li>
+
+      <div className='w-[30px] h-[2px] bg-[#C1C1C1] rotate-90' />
 
       {mosteUsedFilters.map(filter => (
         <MostUsedFilters key={filter.value + filter.name} filter={filter}/>
