bd750746226413bbee6a0404ec5025be87f646c3
Matheus Araujo <matheusfoaraujo.dev@gmail.com>
2024-01-12 16:40:18 -0300

    feat: starts to creat the profile page.

--- app/(private)/profile/page.tsx
+++ app/(private)/profile/page.tsx
@@ -1,4 +1,11 @@
+
+
 import { Metadata } from 'next';
+import ProfileImage from '@/components/Profile/ProfileImage';
+import DeleteAccountButton from '@/components/Profile/DeleteAccountButton';
+import ProfileBox from '@/components/Profile/ProfileBox';
+import PropertyCarousel from '@/components/Profile/PropertyCarousel';
+import RentCarousel from '@/components/Profile/RentCarousel';
 
 export const metadata: Metadata = {
   title: 'Home | Perfil',
@@ -6,8 +13,16 @@ export const metadata: Metadata = {
 
 export default function Profile() {
   return (
-    <section className="text-center">
-      <h1>Profile</h1>
+    <section className="text-center flex-col pb-20 bg-[#F5F5F5]">
+      <div className='w-full h-[125px] bg-gradientBlue'>
+      </div>
+          <ProfileImage/>
+      <div className='mt-20 px-5'>
+        <ProfileBox/>
+      </div>
+      <RentCarousel/>
+      <PropertyCarousel/>
+      <DeleteAccountButton/>
     </section>
   );
 }
---
+++ components/Profile/RentCarousel.tsx
@@ -0,0 +1,27 @@
+"use client"
+import React, { useEffect, useRef } from 'react';
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+
+const RentCarousel: React.FC = () => {
+    const [sliderRef] = useKeenSlider({
+        slides: {
+          perView: 1.5,
+          spacing: 16,
+        },
+        loop:false,
+      });
+  return (
+    <div className='mt-5 ml-5 flex flex-col'>
+        <span className='text-left'>Meus Alugueis</span>
+        <div ref={sliderRef} className="keen-slider mt-2 max-w-[95%]">
+            <div className='max-w-[250px] h-[130px] bg-gray-400 keen-slider__slide'></div>
+            <div className='max-w-[250px] h-[130px] bg-gray-400 keen-slider__slide'></div>
+            <div className='max-w-[250px] h-[130px] bg-gray-400 keen-slider__slide'></div>
+            <div className='max-w-[250px] h-[130px] bg-gray-400 keen-slider__slide'></div>
+        </div>
+    </div>
+  );
+};
+
+export default RentCarousel;
