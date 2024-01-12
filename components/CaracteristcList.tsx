e5da1c754e106a8520b8b55b1bef42dc968ea590
Guilherme Ciarlini <guilhermeciarlini@gmail.com>
2024-01-12 14:24:23 -0300

    feat: recive api infos to apply in caracteristc list

--- components/CaracteristcList.tsx
+++ components/CaracteristcList.tsx
@@ -3,57 +3,63 @@ import { IoBedOutline } from 'react-icons/io5';
 import { FaCar, FaShower } from 'react-icons/fa';
 import { MdPets } from 'react-icons/md';
 
-const caracteristics = {sqrFootage: 40, bedrooms: 1, bathrooms: 3, petFrindly: true, parking: 0}
+interface CaracteristListProps{
+  bathroomsQty: number | undefined,
+  bedroomsQty: number | undefined,
+  parkingQty: number | undefined,
+  sqrFootage: number | undefined,
+  petFriendly: boolean | undefined
+}
 
-export function CaracteristicList(props: any) {
+export function CaracteristicList({bathroomsQty, bedroomsQty, parkingQty, sqrFootage, petFriendly} : CaracteristListProps) {
   return(
-    <>
+    <div className="pl-3 pr-10">
       <h2 className="text-[#ACACAC] text-xs">Caracteristicas</h2>
       
       <ul className="grid grid-cols-3 gap-3">
         <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
             <IoHomeOutline/>
-            <p>Área de {caracteristics.sqrFootage} m²</p>
+            <p>Área de {sqrFootage} m²</p>
         </li>
-        {caracteristics.bedrooms === 0 ?
+        {bedroomsQty === 0 ?
           <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
             <IoBedOutline/>
             <p>sem quartos</p>
           </li>:
           <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
             <IoBedOutline/>
-            <p>{caracteristics.bedrooms} quartos</p>
+            <p>{bedroomsQty} quartos</p>
           </li>
         }
-        {caracteristics.bathrooms === 0 ?
+        {bathroomsQty === 0 ?
           <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
             <FaShower/>
             <p>sem banheiros</p>
           </li>:
           <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
             <FaShower/>
-            <p>{caracteristics.bathrooms} banheiros</p>
+            <p>{bathroomsQty} banheiros</p>
           </li>
         }
 
-        {caracteristics.parking === 0 ?
+        {parkingQty === 0 ?
           <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
             <FaCar/>
             <p>sem vagas</p>
           </li>:
           <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
             <FaCar/>
-            <p>{caracteristics.parking} vagas</p>
+            <p>{parkingQty} vagas</p>
           </li>
         }
-        {caracteristics.petFrindly ?
+        {petFriendly ?
           <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
             <MdPets/>
             <p>Aceita pet</p>
           </li>: null
         }
       </ul>
-    </>
+    </div>
    
   )
 }
