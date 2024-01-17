928300313b151cb68644eea7dcda5089db7cec6f
victormatia <fvictor1705@gmail.com>
2024-01-17 17:13:08 -0300

    refactor: refactor align

--- components/CaracteristcList.tsx
+++ components/CaracteristcList.tsx
@@ -13,47 +13,47 @@ interface CaracteristListProps{
 
 export function CaracteristicList({bathroomsQty, bedroomsQty, parkingQty, sqrFootage, petFriendly} : CaracteristListProps) {
   return(
-    <div className="pl-3 pr-10">
-      <h2 className="text-[#ACACAC] text-xs  min-[700px]:text-xl">Caracteristicas</h2>
+    <div className="basis-[50%]">
+      <h2 className="text-[#ACACAC] text-base font-medium  min-[700px]:text-xl">Caracteristicas</h2>
       
-      <ul className="mt-2 grid grid-cols-3 gap-3">
-        <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
+      <ul className="mt-2 flex flex-wrap text-sm text-grayIcon gap-3">
+        <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center ">
             <IoHomeOutline/>
             <p>Área de {sqrFootage} m²</p>
         </li>
         {bedroomsQty === 0 ?
-          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
+          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center">
             <IoBedOutline/>
             <p>sem quartos</p>
           </li>:
-          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
+          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center">
             <IoBedOutline/>
             <p>{bedroomsQty} quartos</p>
           </li>
         }
         {bathroomsQty === 0 ?
-          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
+          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center">
             <FaShower/>
             <p>sem banheiros</p>
           </li>:
-          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
+          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center">
             <FaShower/>
             <p>{bathroomsQty} banheiros</p>
           </li>
         }
 
         {parkingQty === 0 ?
-          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
+          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center">
             <FaCar/>
             <p>sem vagas</p>
           </li>:
-          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
+          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center">
             <FaCar/>
             <p>{parkingQty} vagas</p>
           </li>
         }
         {petFriendly ?
-          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center text-[10px] text-grayIcon">
+          <li className="border-grayIcon rounded-md border p-2 flex gap-2 items-center justify-center">
             <MdPets/>
             <p>Aceita pet</p>
           </li>: null
