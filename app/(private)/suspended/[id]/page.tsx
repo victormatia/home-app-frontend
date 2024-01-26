049bf085e94e1e1949b23e13c03b38b0f48ae419
Matheus Araujo <matheusfoaraujo.dev@gmail.com>
2024-01-25 21:41:40 -0300

    fix: adds the user data to the account reactivation button

--- app/(private)/suspended/[id]/page.tsx
+++ app/(private)/suspended/[id]/page.tsx
@@ -1,12 +1,19 @@
+'use client'
 import ActivateAccountUserButton from '@/components/Profile/ActivateAccountUserButton'
+import { useUser } from '@auth0/nextjs-auth0/client';
 import React from 'react'
 
 function SuspendedUser() {
+  const { user } = useUser();
+  console.log(user);
+  const daysCount = 999
+  
+
   return (
     <div className='w-full h-screen text-center flex justify-center flex-col items-center min-[700px]:pl-20 max-[700px]:p-4 max-w-[560px] mx-auto'>
       <div className='p-4'>
         <h1 className='font-bold text-red-400  text-4xl text-center'>Conta suspensa!</h1>
-        <p className='mt-4 mb-8 text-left text-lg font-medium  text-red-400'>Olá fulano, sua conta está em processo de suspensão e será deletada da nossa base de dados em: <span className='font-bold'>X dias</span></p>
+        <p className='mt-4 mb-8 text-left text-lg font-medium  text-red-400'>{user?`Olá ${user.given_name}, sua conta está suspensa e será deletada dentro de: ${daysCount} dias!`:''}</p>
         <ActivateAccountUserButton />
       </div>
     </div>
--- components/Profile/ActivateAccountUserButton.tsx
+++ components/Profile/ActivateAccountUserButton.tsx
@@ -25,7 +25,7 @@ const ActivateAccountUserButton: React.FC = () => {
   return (
     <div className='border rounded border-zinc-300 p-5 text-left text-[16px] flex flex-col text-zinc-400'>
       <h1 className='font-bold'>Reativação de conta</h1>
-      <span className='text-opacity-90 mb-3'>Usuário suspenso, para reativar-lo clique no botão abaixo</span>
+      <span className='text-opacity-90 mb-3'>Para evitar que sua conta seja deletada, clique no botão abaixo para reativá-la.</span>
       <button className='text-zinc-600 mt-3 border rounded border-zinc-500 font-bold bg-zinc-200 pt-2 pb-2' onClick={handleActivate}>Reativar Conta</button>
     </div>
   );
