599891a7092d51bc391100a29f323f5a689c83a9
Guilherme Ciarlini <guilhermeciarlini@gmail.com>
2024-01-29 14:29:05 -0300

    feat: create SaveButton component

--- app/(public)/immobile/[id]/page.tsx
+++ app/(public)/immobile/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/Button"
 import { CaracteristicList } from "@/components/CaracteristcList"
 import Map from "@/components/Map"
+import { SaveButton } from "@/components/SaveButton"
 import Slider from "@/components/Slider"
 import { TImmobile } from "@/types"
 import { useQuery } from "@tanstack/react-query"
@@ -29,13 +30,16 @@ export default function Immobile({ params: { id }  } : ImmobileProps ){
       <div className="min-[700px]:left-20 left-0 relative w-full overflow-hidden ">
         <Slider immobile={immobile!}/>
       </div>
-        <div className="text-right w-full max-w-[412px] bg-gradientBlack pr-2 absolute right-0 top-[292px] min-[700px]:hidden">
+        <div className="text-right w-full  bg-gradientBlack pr-2 relative -top-12 right-0">
           <p className="text-[#ACACAC]">Aluguel</p> 
           <p className="text-white">R$ {immobile?.price}</p>
         </div>  
-      <div className="w-full mt-2 mx-auto flex flex-col gap-2 pb-20 min-[700px]:pl-[80px]  min-[700px]:mt-7 min-[700px]:w-[60%] px-3">
+      <div className="w-full -mt-6 mx-auto flex flex-col gap-2 pb-20 min-[700px]:pl-[80px]  min-[700px]:mt-7 min-[700px]:w-[60%] px-3">
         <div>
-          <h2 className="text-[#ACACAC] text-base min-[700px]:text-xl font-medium">Descrição</h2>
+          <div className="flex justify-between">
+            <h2 className="text-[#ACACAC] text-base min-[700px]:text-xl font-medium">Descrição</h2>
+            <SaveButton />
+          </div>
           <p className="text-base font-medium  min-[700px]:text-xl">{ immobile?.description }</p>
         </div>
         <div className="flex flex-col gap-3 min-[700px]:flex-row  min-[700px]:mt-5">
---
+++ components/SaveButton.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Button } from "./Button";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+
+
+export function SaveButton(){
+  const [isSaved, setIsSaved] = useState(false);
+  return(
+    <div>
+
+    {
+      isSaved === false ? (
+        <Button
+        variant='ghost'
+        className='text-grayIcon'
+        onClick={() => setIsSaved(true)}
+        data-testid='bookMarkIcon'
+        >
+          <BsBookmark />
+        </Button>
+      ): (
+        <Button
+        variant='ghost'
+        className='text-grayIcon'
+        onClick={() => setIsSaved(false)}
+        data-testid='bookMarkFillIcon'
+        >
+          <BsBookmarkFill />
+        </Button>
+      )
+    }
+    </div>
+  )
+}
