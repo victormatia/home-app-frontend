914cd389d908171e9b482cfa7cc00ed11ac327e5
Guilherme Ciarlini <guilhermeciarlini@gmail.com>
2024-02-15 15:46:16 -0300

    fix: lint suspended/page.tsx

--- app/(private)/suspended/[id]/page.tsx
+++ app/(private)/suspended/[id]/page.tsx
@@ -1,23 +1,25 @@
-'use client'
-import ActivateAccountUserButton from '@/components/Profile/ActivateAccountUserButton'
+'use client';
+import ActivateAccountUserButton from '@/components/Profile/ActivateAccountUserButton';
 import { useUser } from '@auth0/nextjs-auth0/client';
-import React from 'react'
+import React from 'react';
 
 function SuspendedUser() {
   const { user } = useUser();
   console.log(user);
-  const daysCount = 999
-  
+  const daysCount = 999;
 
   return (
-    <div className='w-full h-screen text-center flex justify-center flex-col items-center max-[700px]:p-4 max-w-[560px] mx-auto'>
+    <div className='mx-auto flex h-screen w-full max-w-[560px] 
+    flex-col items-center justify-center text-center max-[700px]:p-4'>
       <div className='p-4'>
-        <h1 className='font-bold text-red-400  text-4xl text-center'>Conta suspensa!</h1>
-        <p className='mt-4 mb-8 text-left text-lg font-medium  text-red-400'>{user?`Olá ${user.given_name}, sua conta está suspensa e será deletada dentro de: ${daysCount} dias!`:''}</p>
+        <h1 className='text-center text-4xl  font-bold text-red-400'>Conta suspensa!</h1>
+        <p className='mb-8 mt-4 text-left text-lg font-medium  text-red-400'>
+          {user?`Olá ${user.given_name}, sua conta está suspensa e será deletada dentro de: ${daysCount} dias!`:''}
+        </p>
         <ActivateAccountUserButton />
       </div>
     </div>
-  )
+  );
 }
 
 export default SuspendedUser;
