22cff73445fb5fb88cf9059ef51fec86f630ea53
Guilherme Ciarlini <guilhermeciarlini@gmail.com>
2024-02-21 18:46:12 -0300

    fix: lint AdvertiseActions.tsx

--- components/AdvertiseActions.tsx
+++ components/AdvertiseActions.tsx
@@ -1,35 +1,35 @@
-'use client'
+'use client';
 
-import globalContext from "@/context/context"
-import { useContext, useEffect } from "react"
-import { Button } from "./Button";
-import Link from "next/link";
+import globalContext from '@/context/context';
+import { useContext, useEffect } from 'react';
+import { Button } from './Button';
+import Link from 'next/link';
 
 function AdvertiseActions() {
   const { setCurrPage } = useContext(globalContext);
 
-  useEffect(() => setCurrPage("advertise") , []);
+  useEffect(() => setCurrPage('advertise') , []);
 
   return (
-    <div className='flex gap-2 justify-center p-4'>
-        <Button
-          variant='ghost'
-          className='p-4 border border-zinc-200 hover:shadow-default transition-all'
-        >
-          <Link href='http://localhost:3000/advertise/propertyRegister'>
+    <div className='flex justify-center gap-2 p-4'>
+      <Button
+        variant='ghost'
+        className='hover:shadow-default border border-zinc-200 p-4 transition-all'
+      >
+        <Link href='http://localhost:3000/advertise/propertyRegister'>
             Cadastrar imóvel
-          </Link>
-        </Button>
-        <Button
-          variant='ghost'
-          className='p-4 border border-zinc-200 hover:shadow-default transition-all'
-        >
-          <Link href=''>
+        </Link>
+      </Button>
+      <Button
+        variant='ghost'
+        className='hover:shadow-default border border-zinc-200 p-4 transition-all'
+      >
+        <Link href=''>
             Gerenciar imóveis
-          </Link>
-        </Button>
-      </div>
-  )
+        </Link>
+      </Button>
+    </div>
+  );
 }
 
-export default AdvertiseActions
+export default AdvertiseActions;
